9ed5e054711173f32420103ab56824eab84e0ec5
xieyimaple <xieyi_maple@163.com>
2016-09-07 14:18:45 +0800

    学校信息管理系统

---
+++ 学校信息管理系统/model/clazz/classes.js
@@ -0,0 +1,7 @@
+classData([
+{"id":1,"classname":"一班","classnumber":36,"classteacher":"潘金莲","monitor":"张三丰"},
+{"id":2,"classname":"二班","classnumber":44,"classteacher":"西门庆","monitor":"赵铁柱"},
+{"id":3,"classname":"三班","classnumber":38,"classteacher":"王五","monitor":"隔壁王"},
+{"id":4,"classname":"四班","classnumber":41,"classteacher":"李四","monitor":"冰冰"},
+{"id":5,"classname":"五班","classnumber":42,"classteacher":"张三","monitor":"冷冷"}
+]);
